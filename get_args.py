f55902a7b91e80b47dfe0c4e648b6ed244c96625
Perri Goh <wmxpg@singnet.com.sg>
2022-10-26 03:33:56 +0800

    fix: allows command line arguments

--- get_args.py
+++ get_args.py
@@ -39,21 +39,21 @@ def get_args():
     # arguments for train.py
     # Option: python train.py data_dir --save_dir save_directory
     parser.add_argument('--save_dir', dest='save_dir', type=str, default='checkpoint.pth', 
-                    help='Set directory to save checkpoint')
+                    help='Set directory to save checkpoint') 
     # Option: python train.py data_dir --arch "vgg13"
-    parser.add_argument('--arch', dest='arch', type=str, default='vgg13', 
+    parser.add_argument('--arch', dest='arch', type=str, 
                     help='Choose architecture, alexnet, vgg13 or vgg16')
     # Option: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
-    parser.add_argument('--learning_rate', dest='learning_rate', type=float, default='0.01', 
+    parser.add_argument('--learning_rate', dest='learning_rate', type=float, 
                     help='Set hyperparameters - learning rate type=float eg. 0.01')
-    parser.add_argument('--hidden_units', dest='hidden_units', type = int, default='512', 
+    parser.add_argument('--hidden_units', dest='hidden_units', type = int, 
                     help='Set hyperparameters - hidden_units type=int range 102 - 9216')
-    parser.add_argument('--epochs', dest='epochs', type=int, default='1', 
+    parser.add_argument('--epochs', dest='epochs', type=int, 
                     help='Set hyperparameters - epochs')
 
     # arguments for predict.py
     # Option: python predict.py input checkpoint --topk_3
-    parser.add_argument('--topk', dest='topk', type=int, default='3', 
+    parser.add_argument('--topk', dest='topk', type=int, 
                     help = 'Top K most likely classes')
     # Option: python predict.py input checkpoint --category_names cat_to_name.json
     parser.add_argument('--category_names', dest='category_names', type=str, 
