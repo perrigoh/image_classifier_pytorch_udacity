1c59ee570df9ea02d8e8de94fa77687dd91bed03
Perri Goh <wmxpg@singnet.com.sg>
2022-10-26 14:34:02 +0800

    fix: program run on default arg if not specified

--- get_args.py
+++ get_args.py
@@ -18,12 +18,12 @@ def get_args():
     then the default values are used for the missing arguments.
     Command Line Arguments:
     1. Directory to save checkpoints as --save_dir with default value 'checkpoint.pth'
-    2. Model Architecture as --arch with default value 'vgg13'
+    2. Model Architecture as --arch 
     Hyperparameters:
-    3. Learning Rate as --learning_rate with default value '0.01'
-    4. Hidden Units as --hidden_units with default value '512'
-    5. Epochs as --epochs with default value '20'
-    6. Top K as --top_k with default value '3'
+    3. Learning Rate as --learning_rate
+    4. Hidden Units as --hidden_units
+    5. Epochs as --epochs
+    6. Top K as --top_k
     7. Mapping of categories to real names as --category_names with default 
        value 'cat_to_name.json'
     This function returns these arguments as an ArgumentParser object.
@@ -40,10 +40,10 @@ def get_args():
     # Option: python train.py data_dir --save_dir save_directory
     parser.add_argument('--save_dir', dest='save_dir', type=str, default='checkpoint.pth', 
                     help='Set directory to save checkpoint') 
-    # Option: python train.py data_dir --arch "vgg13"
+    # Option: python train.py data_dir --arch
     parser.add_argument('--arch', dest='arch', type=str, 
                     help='Choose architecture, alexnet, vgg13 or vgg16')
-    # Option: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
+    # Option: python train.py data_dir --learning_rate --hidden_units --epochs
     parser.add_argument('--learning_rate', dest='learning_rate', type=float, 
                     help='Set hyperparameters - learning rate type=float eg. 0.01')
     parser.add_argument('--hidden_units', dest='hidden_units', type = int, 
@@ -52,7 +52,7 @@ def get_args():
                     help='Set hyperparameters - epochs')
 
     # arguments for predict.py
-    # Option: python predict.py input checkpoint --topk_3
+    # Option: python predict.py input checkpoint --topk
     parser.add_argument('--topk', dest='topk', type=int, 
                     help = 'Top K most likely classes')
     # Option: python predict.py input checkpoint --category_names cat_to_name.json
