8bd3adf396fef863da37ed2a1938b08dd3252ffa
Perri Goh <wmxpg@singnet.com.sg>
2022-10-29 17:37:39 +0800

    fix: run command with/without flag and arg train.py and predict.py able to run command with/without flag and argument

--- get_args.py
+++ get_args.py
@@ -2,8 +2,8 @@
 # 
 # PROGRAMMER: Perri Goh
 # DATE CREATED: 4 Oct 2022                                  
-# REVISED DATE: 26 Oct 2022
-# 
+# REVISED DATE: 29 Oct 2022
+#-----------------------50-----------------------|----------72---------| 
 
 # Imports python module
 import argparse
@@ -11,19 +11,21 @@ import argparse
 def get_args():
 
     """
-    Retrieves and parse the command line arguments provided by the user
-    when they run the progammer from a terminal window. This function use 
-    Python's argparse module to create and define these command line
-    arguments. If the user fails to provide some or all of the arguments,
-    then the default values are used for the missing arguments.
+    Retrieves and parses the command line arguments provided by the user
+    when they run the programme from a terminal window. This function uses 
+    Python's argparse module to create and define command line arguments. 
+    If the user provides a flag without argument, the const value will be
+    used as the argument. If the user fails to provide some or all the 
+    arguments, then the default values are used for the missing arguments.
     Command Line Arguments:
-    1. Directory to save checkpoints as --save_dir with default value 'checkpoint.pth'
-    2. Model Architecture as --arch 
+    1. Directory to save checkpoints as --save_dir with default value 
+       'checkpoint.pth'
+    2. Model Architecture as --arch with default value 'vgg13'
     Hyperparameters:
-    3. Learning Rate as --learning_rate
-    4. Hidden Units as --hidden_units
-    5. Epochs as --epochs
-    6. Top K as --top_k
+    3. Learning Rate as --learning_rate with default value 0.01
+    4. Hidden Units as --hidden_units  with default value 512
+    5. Epochs as --epochs with default value 1
+    6. Top K as --top_k  with default value 3
     7. Mapping of categories to real names as --category_names with default 
        value 'cat_to_name.json'
     This function returns these arguments as an ArgumentParser object.
@@ -38,28 +40,32 @@ def get_args():
 
     # arguments for train.py
     # Option: python train.py data_dir --save_dir save_directory
-    parser.add_argument('--save_dir', dest='save_dir', type=str, default='checkpoint.pth', 
-                    help='Set directory to save checkpoint') 
+    parser.add_argument('--save_dir', dest='save_dir', nargs='?', type=str,
+                        const='checkpoint.pth', default='checkpoint.pth', 
+                        help='Set directory to save checkpoint') 
     # Option: python train.py data_dir --arch
-    parser.add_argument('--arch', dest='arch', type=str, 
-                    help='Choose architecture, alexnet, vgg13 or vgg16')
+    parser.add_argument('--arch', dest='arch', nargs='?', type=str,
+                        const='vgg13', default='vgg13', 
+                        help='Choose architecture, alexnet, vgg13 or vgg16')
     # Option: python train.py data_dir --learning_rate --hidden_units --epochs
-    parser.add_argument('--learning_rate', dest='learning_rate', type=float, 
-                    help='Set hyperparameters - learning rate type=float eg. 0.01')
-    parser.add_argument('--hidden_units', dest='hidden_units', type = int, 
-                    help='Set hyperparameters - hidden_units type=int range 102 - 9216')
-    parser.add_argument('--epochs', dest='epochs', type=int, 
-                    help='Set hyperparameters - epochs')
+    parser.add_argument('--learning_rate', dest='learning_rate', nargs='?',
+                        type=float, const=0.01, default=0.01,
+                        help='Set hyperparameters - learning rate type=float eg. 0.01')
+    parser.add_argument('--hidden_units', dest='hidden_units', nargs='?',
+                        type = int, const=512, default=512,
+                        help='Set hyperparameters - hidden_units type=int range 102 - 9216')
+    parser.add_argument('--epochs', dest='epochs', nargs='?', type=int, 
+                        const=1, default=1,
+                        help='Set hyperparameters - epochs')
 
     # arguments for predict.py
     # Option: python predict.py input checkpoint --topk
-    parser.add_argument('--topk', dest='topk', type=int, 
-                    help = 'Top K most likely classes')
+    parser.add_argument('--topk', dest='topk', nargs='?', type=int,
+                        const=3, default=3, 
+                        help = 'Top K most likely classes')
     # Option: python predict.py input checkpoint --category_names cat_to_name.json
-    parser.add_argument('--category_names', dest='category_names', type=str, 
-                    default='cat_to_name.json', 
-                    help='A list of category names in json format')
+    parser.add_argument('--category_names', dest='category_names', nargs='?',
+                        type=str, const='cat_to_name.json', default='cat_to_name.json', 
+                        help='A list of category names in json format')
 
     return parser.parse_args()
-
-
